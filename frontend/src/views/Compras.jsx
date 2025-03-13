acd2bf784fbc05b83563a08f11afd8b286ffa996
FranciscoYuster <yusterfrancisco@gmail.com>
2025-03-13 17:17:26 -0300

    add compras.jsx

--- frontend/src/views/Compras.jsx
+++ frontend/src/views/Compras.jsx
@@ -1,158 +1,168 @@
-import { useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Table, Form, InputGroup, Pagination } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function EditableTable() {
-  const [rows, setRows] = useState([
-    {
-      id: 1,
-      cantidad: "",
-      articulo: "",
-      tipo: "",
-      número: "",
-      proveedor: "",
-      precio: "",
-      total: "",
-    },
-  ]);
 
-  const addRow = () => {
-    setRows([
-      ...rows,
-      {
-        id: rows.length + 1,
-        cantidad: "",
-        articulo: "",
-        tipo: "",
-        número: "",
-        proveedor: "",
-        precio: "",
+const Compras = () => {
+
+  const [purchases, setPurchase] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPurchases, setSelectedPurchases] = useState([]);
+  cosnt[editPurchase, setEditPurchase] = useState(null)
+  cosnt[deletePurchase, setDeletePurchase] = useState(null)
+  cosnt[deletePurchase, setDeletePurchase] = useState(null)
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  const [showDeleteAllConfirmation, setShowDeleteAllConfirmation] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const itemsPerPage = 10;
+
+  // Estado para una nueva compra
+
+  const [newPurchase, setNewPurchase] = useState({
+    numero_comprobante: "",
+    orden_compra: "",
+    metodo: "",
+    provider_id: "",
+    product_id: "",
+    inventory_id: "",
+    quantity: "",
+    total: "",
+    status: "",
+    type: ""
+  });
+
+
+  const token = sessionStorage.getItem("access_token");
+
+  useEffect(() => {
+    fetchPurchases();
+  }, [])
+
+  const fetchPurchases = () => {
+    fetch("/api/purchases", {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error HTTP: ${response.status}`);
+        }
+        return response.json()
+      })
+      .then(data => setPurchase(data))
+      .catch((error) => {
+        console.error("Error al obtener compras", err);
+        toast.error("Error al cargar compras!")
+      })
+  }
+
+  // Filtrado y paginacion
+
+  const filteredPurchases = purchases.filter(purchase =>
+    purchase.orden_compra.toLowerCase().includes(searchQuery.toLowerCase())
+  )
+
+  const totalPages = Math.ceil(filteredPurchases.length / itemsPerPage);
+  const currentItems = filteredPurchases.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
+  const handleSelectPurchases = (id) => {
+    if (selectedPurchases.includes(id)) {
+      setSelectedPurchases(selectedPurchases.filter(purchases => purchases !== id))
+    } else {
+      setSelectedPurchases([...selectedPurchases, id])
+    }
+  }
+
+  const handleSelectAll = () => {
+    if (selectedPurchases.length === currentItems.length) {
+      setSelectedPurchases([]);
+    } else {
+      setSelectedPurchases(currentItems.map((producto) => producto.id));
+    }
+  };
+
+  // Abrir modal
+
+  const handleShowModal = (purchases = null) => {
+    setEditPurchase(purchases);
+    if (purchases) {
+      setNewPurchase({
+        numero_comprobante: purchases.invoice && purchases.invoice.numero_comprobante ? purchases.invoice.numero_comprobante : "",
+        orden_compra: purchases.orden_compra,
+        metodo: purchases.metodo,
+        provider_id: purchases.provider_id,
+        product_id: purchases.product_id,
+        inventory_id: purchases.inventory_id,
+        quantity: purchases.quantity,
+        total: purchases.total,
+        status: purchases.status,
+        type: purchases.type
+      })
+    } else {
+      setNewPurchase({
+        numero_comprobante: "",
+        orden_compra: "",
+        metodo: "",
+        provider_id: "",
+        product_id: "",
+        inventory_id: "",
+        quantity: "",
         total: "",
-      },
-    ]);
+        status: "",
+        type: ""
+      })
+    }
+    setShowModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setEditPurchase(null);
+    setNewPurchase({
+      numero_comprobante: "",
+      orden_compra: "",
+      metodo: "",
+      provider_id: "",
+      product_id: "",
+      inventory_id: "",
+      quantity: "",
+      total: "",
+      status: "",
+      type: ""
+    })
   };
 
-  const deleteRow = (id) => {
-    setRows(rows.filter((row) => row.id !== id));
-  };
 
-  const handleChange = (id, field, value) => {
-    setRows(
-      rows.map((row) => (row.id === id ? { ...row, [field]: value } : row))
-    );
-  };
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
-    <div className="container mt-4">
-      <table className="table table-bordered">
-        <thead className="table-dark">
-          <h1>Compras</h1>
-          <tr>
-            <th>#</th>
-            <th>Cantidad</th>
-            <th>Articulo</th>
-            <th>Tipo de ingreso </th>
-            <th>Número de comprobante</th>
-            <th>Proveedor</th>
-            <th>Precio</th>
-            <th>total</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, index) => (
-            <tr key={row.id}>
-              <td>{index + 1}</td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.cantidad}
-                  onChange={(e) =>
-                    handleChange(row.id, "cantidad", e.target.value)
-                  }
-                  placeholder="Cantidad"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.articulo}
-                  onChange={(e) =>
-                    handleChange(row.id, "articulo", e.target.value)
-                  }
-                  placeholder="Articulo"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.tipo}
-                  onChange={(e) => handleChange(row.id, "tipo", e.target.value)}
-                  placeholder="Tipo de ingreso"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.número}
-                  onChange={(e) =>
-                    handleChange(row.id, "número", e.target.value)
-                  }
-                  placeholder="Número de comprobante"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.proveedor}
-                  onChange={(e) =>
-                    handleChange(row.id, "proveedor", e.target.value)
-                  }
-                  placeholder="Proveedor"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.precio}
-                  onChange={(e) =>
-                    handleChange(row.id, "precio", e.target.value)
-                  }
-                  placeholder="Precio"
-                />
-              </td>
-              <td>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={row.total}
-                  onChange={(e) =>
-                    handleChange(row.id, "total", e.target.value)
-                  }
-                  placeholder="total"
-                />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteRow(row.id)}
-                >
-                  <FaTrash />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button className="btn btn-primary" onClick={addRow}>
-        Add new row
-      </button>
-    </div>
-  );
+    <div>Compras</div>
+  )
 }
+
+export default Compras
